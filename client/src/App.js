637986d7ccd4eaa20c1d4df869c9b260961c8678
Joseph <jtawil2015@gmail.com>
2020-08-26 21:04:07 -0800

    creating chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
-import React,{useEffect} from 'react';
+import React,{useEffect, useState} from 'react';
 import logo from './logo.svg';
+import io from 'socket.io-client'
+import TextField from '@material-ui/core/TextField'
 import axios from 'axios';
 import './App.css';
 
+const scoket = io.connect('http://localhost:4000');
+
 //comment
 function App() {
+
+  const [state, setState] = useState({message: '', name: ''});
+  const [chat, setChat] = useState([]);
+
+
+  const renderChat = () =>{
+    return chat.map({name, message}, index=>(
+      <div key={index}>
+        <h3>
+          {name}: <span>{message}</span>
+        </h3>
+      </div>
+    ))
+  }
+
 
   useEffect(() => {
     axios.get(`/example`).then((res)=>console.log(res));
@@ -12,20 +31,20 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onMessageSubmit}>
+        <h1>Messenger</h1>
+        <div className="name-field">
+          <TextField name="name" onChange={e => onTextChange(e)} value = {state.name} label = "Name"/>
+        </div>
+        <div>
+          <TextField name="message" onChange={e => onTextChange(e)} value = {state.message} label = "Message"/>
+        </div>
+        <button>Send Message</button>
+      </form>
+      <div className="render-chat">
+        <h1>Chat Log</h1>
+        {renderChat()}
+      </div>
     </div>
   );
 }
