38048c3f5e1021ce1c426e6bdf3677e9f561329c
ichoi21 <ihchoi@gmail.com>
2020-09-02 21:12:05 -0700

    backend auth update

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,6 @@
-const express = require("express");
+const express = require("express").Router();
+const Axios = require("axios");
+const auth = require("../middleware/auth");
 const router = express.Router();
 const path = require("path");
 const db = require("../models");
@@ -8,25 +10,26 @@ router.get("/example", (req, res) => {
 });
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/', 'index.html'));
+  res.sendFile(path.join(__dirname, "../client/public/", "index.html"));
 });
 
-
 router.get("/signup", (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/', 'signup.html'));
+  res.sendFile(path.join(__dirname, "../client/public/", "signup.html"));
 });
 
 router.post("/signup", (req, res) => {
-  db.User.create({ 
+  db.User.create({
     first_name: req.body.first,
     last_name: req.body.last,
     email: req.body.email,
     password: req.body.password,
   })
-  .then(function(dbUser) {
+    .then(function (dbUser) {
       res.send(dbUser);
-  })
-  .catch(function(err) { res.json(err); });
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
 });
 
 module.exports = router;
