27e5808f8ea7d1a75fb5f8413adc9d9feb1c1b58
Danielle Arias <daniellearias321@gmail.com>
2020-09-02 21:54:35 -0700

    added quiz models and routes

---
+++ models/Quiz.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const QuizSchema = new Schema({
+    question: {type: String, required: true}, 
+    answers: [String]
+    content: {type: String, required:true}
+});
+
+const Quiz = mongoose.model("User", UserSchema);
+
+module.exports = Quiz;
