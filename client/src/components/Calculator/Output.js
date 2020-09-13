14c0fbdf9ceea1929ce43485a2ca718680cdec66
ichoi21 <ihchoi@gmail.com>
2020-09-12 18:03:30 -0700

    BMI calc is functional

--- client/src/components/Calculator/Output.js
+++ client/src/components/Calculator/Output.js
@@ -1,39 +1,44 @@
 import React, { Component } from "react";
 
-class Range extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: props.value,
-    };
-  }
-
-  onChange = (e) => {
-    //pass state to parent component in index.js
-    this.props.onChange(this.state.value);
-    this.setState({ value: e.target.value });
+class Output extends Component {
+  // convert cm into ft
+  toFeet = (num) => {
+    let realFeet = (num * 0.3937) / 12;
+    let feet = Math.floor(realFeet);
+    let inches = Math.round((realFeet - feet) * 12);
+    return `${feet}'${inches}`;
   };
-
-  static defaultProps = {
-    min: 0,
-    max: 245,
-    step: 1,
+  // convert kg to lbs
+  toLbs = (num) => {
+    let nearExact = num / 0.45359237;
+    let lbs = Math.floor(nearExact);
+    return lbs;
   };
 
   render() {
+    let height = this.props.data.height;
+    let weight = this.props.data.weight;
+    let bmi = this.props.data.bmi;
+    let bmiClass = this.props.data.bmiClass;
+    // conversions
+    let heightFeet = this.toFeet(height);
+    let pounds = this.toLbs(weight);
+
     return (
-      <div className="range">
-        <input
-          type="range"
-          value={this.state.value}
-          min={this.props.min}
-          max={this.props.max}
-          step={this.props.step}
-          onChange={this.onChange}
-        />
+      <div className="output">
+        <h3>
+          {height}cm
+          <span className="imperial"> {heightFeet}</span>
+        </h3>
+        <h3>
+          {weight}kg
+          <span className="imperial"> {pounds}lbs</span>
+        </h3>
+        <h3>{bmi}</h3>
+        <h3>{bmiClass}</h3>
       </div>
     );
   }
 }
 
-export default Range;
+export default Output;
