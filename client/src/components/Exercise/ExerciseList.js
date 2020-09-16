e5175cf4a0d4e8ae2e530571bf53c2e0abbe8d04
Joseph <jtawil2015@gmail.com>
2020-09-16 12:45:30 -0800

    grabbing exercises from api

--- client/src/components/Exercise/ExerciseList.js
+++ client/src/components/Exercise/ExerciseList.js
@@ -1,20 +1,48 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import Exercise from './Exercises';
-
+import axios from 'axios';
 import {BrowserRouter as Router, Switch, Link, Route, useParams} from 'react-router-dom';
 const ExerciseList = () => {
+  
+        const [state, setState] = useState({name: []});
+        let {muscles} = useParams();
+        console.log(muscles);
 
+        const getExercises = ()=> {
+            axios.get(`https://wger.de/api/v2/exerciseinfo/?limit=100&offset=20`).then((res)=>{
+        console.log(res.data.results);
+        const categ = res.data.results;
+         categ.map((ex)=>{
+             console.log(ex.category.name)
+            if(ex.category.name===muscles)
+            {
+                if(ex.name != ""){
+                    setState({
+                        name: ex.name
+                    })
+                }
+            }
+        })
+       });
+        }
+         
+        useEffect(() => {
+            getExercises();
+        }, [])
+    
 
-    function Category() {
-        let {category} = useParams();
-        return <div>This is {category} category</div>
-    }
     return (
         <div>
             <Router>
                 <Switch>
-                    <Route path="/exercises/:category">
-                        <Category/>
+                    <Route path="/exercises/:muscles">
+                        <Exercise/>
+                        <h1>This is {muscles}</h1>
+                       <div className="container">
+                       {state.name.map((exercises)=>{
+                            console.log(exercises);
+                        })}
+                           </div> 
                     </Route>
                 </Switch>
             </Router>
--- client/src/handlers/PrivateRoute.js
+++ client/src/handlers/PrivateRoute.js
@@ -6,15 +6,15 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
 
     const { auth } = useAuthContext();
     console.log("proute", auth)
-    if (auth.loading) return <p> Loading </p>
-    return (
-        <Route {...rest} render={(props) => (
-            // localStorage.getItem('token') !== null
-            auth.isAuthenticated
-                ? <Component {...props} />
-                : <Redirect to='/login' />
-        )} />
-    )
+    // if (auth.loading) return <p> Loading </p>
+    // return (
+    //     <Route {...rest} render={(props) => (
+    //         // localStorage.getItem('token') !== null
+    //         auth.isAuthenticated
+    //             ? <Component {...props} />
+    //             : <Redirect to='/login' />
+    //     )} />
+    // )
 };
 
 export default PrivateRoute;
