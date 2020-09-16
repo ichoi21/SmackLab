6adad9dd5574514ac315c0f6eae8ba1e5571dbb6
ichoi21 <ihchoi@gmail.com>
2020-09-16 02:08:59 -0700

    updated About page

---
+++ client/src/components/About/AboutBox.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./box.css";
+
+const details = (props) => (
+  <div className="profilebox">
+    <div className="profileImage">
+      <img src={props.profile.image} alt="profile" />
+    </div>
+    <div className="profileinfo">
+      <h4 className="font15 weight800">{props.profile.name}</h4>
+      <p className="font12 weight500 padding10">{props.profile.description}</p>
+      <p className="font12 weight500 rankInfo shadow">{props.profile.rank}</p>
+    </div>
+  </div>
+);
+
+export default details;
