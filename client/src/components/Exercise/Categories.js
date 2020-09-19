83c5317b44f3bfe73ff09ee1544d96c914a4118f
ichoi21 <ihchoi@gmail.com>
2020-09-19 08:17:04 -0700

    keys update

--- client/src/components/Exercise/Categories.js
+++ client/src/components/Exercise/Categories.js
@@ -1,72 +1,64 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./styling/Categories.css";
 import pic0 from "../../images/pic0.jpg";
-import pic1 from "../../images/pic1.jpg"
-import pic2 from "../../images/pic2.jpg"
-import pic3 from "../../images/pic3.jpg"
-import pic4 from "../../images/pic4.jpg"
-import pic5 from "../../images/pic5.jpg"
-import pic6 from "../../images/pic6.jpg"
+import pic1 from "../../images/pic1.jpg";
+import pic2 from "../../images/pic2.jpg";
+import pic3 from "../../images/pic3.jpg";
+import pic4 from "../../images/pic4.jpg";
+import pic5 from "../../images/pic5.jpg";
+import pic6 from "../../images/pic6.jpg";
 const picArray = [pic0, pic1, pic2, pic3, pic4, pic5, pic6];
 
 const api = axios.create({
-    baseURL: "https://wger.de/api/v2/exercisecategory/"
+  baseURL: "https://wger.de/api/v2/exercisecategory/",
 });
 
 // const api = axios.get("https://wger.de/api/v2/exercisecategory/");
 const image = axios.create({
-    baseURL: "https://wger.de/api/v2/exerciseimage/"
+  baseURL: "https://wger.de/api/v2/exerciseimage/",
 });
 
 const Exercises = () => {
-    
-    const [state, setState] = useState({name:[], image: []});
+  const [state, setState] = useState({ name: [], image: [] });
 
-    const getExercises = (exercises) => {
-        window.location.href = `http://localhost:3000/exercises/${exercises.name}`;
-    }
+  const getExercises = (exercises) => {
+    window.location.href = `exercises/${exercises.name}`;
+  };
 
-
-    const getMuscles = ()=>{
-
-        api.get('/').then(res=>{
-            console.log(res.data.results);
-            image.get('/').then(img=>{
-                console.log(img.data.results);
-                setState( {
-                    name: res.data.results,
-                    image: img.data.results,
-                });
-            });
+  const getMuscles = () => {
+    api.get("/").then((res) => {
+      console.log(res.data.results);
+      image.get("/").then((img) => {
+        console.log(img.data.results);
+        setState({
+          name: res.data.results,
+          image: img.data.results,
         });
-    }
+      });
+    });
+  };
 
-    useEffect(() => {
-        getMuscles();
-    }, []);
+  useEffect(() => {
+    getMuscles();
+  }, []);
 
-   
-    return (
-        <div className="container" style={{border: '1px solid'}}>
-            
-            {state.name.map((exercises, index) => (
-                
-                <div class="row">
-                <div class="col s12 m6">
-                  <div class="card" onClick={() => getExercises(exercises)}>
-                    <div class="card-image">
-                      <img src={picArray[index]}/>
-                      <span class="card-title" >{exercises.name}</span>
-                    </div>
-                  </div>
-                </div>
+  return (
+    <div className="container" style={{ border: "1px solid" }}>
+      {state.name.map((exercises, index) => (
+        <div class="row">
+          <div class="col s12 m6">
+            <div class="card" onClick={() => getExercises(exercises)}>
+              <div class="card-image">
+                <img src={picArray[index]} />
+                <span class="card-title">{exercises.name}</span>
               </div>
-                     
-                )        
-            )}
+            </div>
+          </div>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default Exercises
+export default Exercises;
--- client/src/components/Exercise/ExerciseList.js
+++ client/src/components/Exercise/ExerciseList.js
@@ -6,67 +6,66 @@ import Video from "./Video";
 import ExerciseCard from "./Card/ExerciseCard";
 import { Grid } from "@material-ui/core";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Link,
-    Route,
-    useParams,
+  BrowserRouter as Router,
+  Switch,
+  Link,
+  Route,
+  useParams,
 } from "react-router-dom";
 import "../Pages/Styles.css";
 import "./ExerciseList.css";
 
 const ExerciseList = () => {
-    let parts = [];
-    const [state, setState] = useState({ name: [] });
-    const [bodyParts, setBodyParts] = useState({ body: [] });
-    let { muscles } = useParams();
+  let parts = [];
+  const [state, setState] = useState({ name: [] });
+  const [bodyParts, setBodyParts] = useState({ body: [] });
+  let { muscles } = useParams();
 
-    const getExercises = () => {
-        allExercises.map((exercise) => {
-            if (exercise[0] === muscles) {
-                exercise.splice(0, 1);
-                exercise.map((muscle) => {
-                    console.log(muscle);
-                    parts.push(muscle);
-                });
-            }
-
-            setBodyParts({ body: parts });
+  const getExercises = () => {
+    allExercises.map((exercise) => {
+      if (exercise[0] === muscles) {
+        exercise.splice(0, 1);
+        exercise.map((muscle) => {
+          console.log(muscle);
+          parts.push(muscle);
         });
-    };
+      }
 
-    useEffect(() => {
-        getExercises();
-    }, []);
+      setBodyParts({ body: parts });
+    });
+  };
 
-    return (
-        <div className="exerciseList">
-            <Router>
-                <Switch>
-                    <Route path="/exercises/:muscles">
-                        {/* <Exercise/> */}
-                        <h1 className="weight800 font40">This is {muscles}</h1>
-                        <div className="Techniques">
-                            <Grid container className="Techniques2" spacing={4}>
-                                {bodyParts.body.map((exercise) => {
-                                    console.log(exercise);
-                                    return (
-                                        <Grid item xs={4}>
-                                            <ExerciseCard
-                                                name={exercise}
-                                                exercise={exercise}
-                                                text={muscles}
-                                            />
-                                        </Grid>
-                                    );
-                                })}
-                            </Grid>
-                        </div>
-                    </Route>
-                </Switch>
-            </Router>
-        </div>
-    );
+  useEffect(() => {
+    getExercises();
+  }, []);
+
+  return (
+    <div className="exerciseList">
+      <Router>
+        <Switch>
+          <Route path="/exercises/:muscles">
+            <h1 className="weight800 font40">This is {muscles}</h1>
+            <div className="Techniques">
+              <Grid container className="Techniques2" spacing={4}>
+                {bodyParts.body.map((exercise) => {
+                  console.log(exercise);
+                  return (
+                    <Grid item xs={4}>
+                      <ExerciseCard
+                        name={exercise}
+                        exercise={exercise}
+                        text={muscles}
+                      />
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default ExerciseList;
